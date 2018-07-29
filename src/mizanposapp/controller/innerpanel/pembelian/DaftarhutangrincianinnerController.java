759a9e35aa172217f19383f31c4524e665cd7d29
mayonklepas <imonks21@gmail.com>
2018-07-29 22:20:10 +0800

    Merge origin/master

--- src/mizanposapp/controller/innerpanel/pembelian/DaftarhutangrincianinnerController.java
+++ src/mizanposapp/controller/innerpanel/pembelian/DaftarhutangrincianinnerController.java
@@ -48,7 +48,7 @@ import org.json.simple.parser.ParseException;
  * @author Minami
  */
 public class DaftarhutangrincianinnerController {
-    
+
     CrudHelper ch = new CrudHelper();
     ArrayList<String> idlist = new ArrayList<>();
     ArrayList<String> lsdata = new ArrayList();
@@ -62,7 +62,7 @@ public class DaftarhutangrincianinnerController {
     Daftarhutangrincian_inner_panel pane;
     String id = "";
     SwingWorker worker;
-    
+
     public DaftarhutangrincianinnerController(Daftarhutangrincian_inner_panel pane) {
         this.pane = pane;
         id = Staticvar.ids;
@@ -87,9 +87,9 @@ public class DaftarhutangrincianinnerController {
         oncarienter();
         onbuttoncari();
         deleterincian();
-        
+
     }
-    
+
     private void loadheader() {
         try {
             pane.tabledata.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -112,7 +112,7 @@ public class DaftarhutangrincianinnerController {
                     lssize.add(Integer.parseInt(String.valueOf(jaaray.get(3))));
                 }
             }
-            
+
             for (int i = 0; i < lssize.size(); i++) {
                 Double wd = d.getWidth() - 344;
                 int wi = (lssize.get(i) * wd.intValue()) / 100;
@@ -123,7 +123,7 @@ public class DaftarhutangrincianinnerController {
             Logger.getLogger(DaftarhutangrincianinnerController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     private void loadheaderrincian() {
         try {
             pane.tabledatarincian.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -149,7 +149,7 @@ public class DaftarhutangrincianinnerController {
                     lssizerincian.add(Integer.parseInt(String.valueOf(jaaray.get(3))));
                 }
             }
-            
+
             for (int i = 0; i < lssizerincian.size(); i++) {
                 Double wd = d.getWidth() - 344;
                 int wi = (lssizerincian.get(i) * wd.intValue()) / 100;
@@ -160,7 +160,7 @@ public class DaftarhutangrincianinnerController {
             Logger.getLogger(DaftarhutangrincianinnerController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     private void loaddata(int row) {
         cleardata();
         disablebutton();
@@ -186,7 +186,7 @@ public class DaftarhutangrincianinnerController {
                 }
                 return null;
             }
-            
+
             @Override
             protected void done() {
                 pane.indi.setVisible(false);
@@ -195,12 +195,12 @@ public class DaftarhutangrincianinnerController {
                 pane.tabledata.requestFocus();
                 pane.tabledata.changeSelection(row, 0, false, false);
             }
-            
+
         };
         worker.execute();
-        
+
     }
-    
+
     private void loaddatadetailraw() {
         cleardata2();
         disablebutton();
@@ -224,84 +224,102 @@ public class DaftarhutangrincianinnerController {
                     }
                     dtm.addRow(objindata);
                 }
-                
+
                 return null;
             }
-            
+
             @Override
             protected void done() {
                 pane.indi.setVisible(false);
                 pane.tabledata.setModel(dtm);
                 disablebutton();
-                
+
             }
-            
+
         };
         worker.execute();
-        
+
     }
-    
+
     private void loaddatadetailrincian(String id) {
-        cleardata2();
-        dtmrincian.getDataVector().removeAllElements();
-        dtmrincian.fireTableDataChanged();
-        worker = new SwingWorker<Void, Void>() {
+        try {
+            cleardata2();
+            dtmrincian.getDataVector().removeAllElements();
+            dtmrincian.fireTableDataChanged();
+            JSONParser jpdata = new JSONParser();
+            String param = String.format("id=%s", id);
+            Object objdata = jpdata.parse(ch.getdatadetails("daftarcicilanhutangpersupplier", param));
+            JSONArray jadata = (JSONArray) objdata;
+            dtmrincian.setRowCount(0);
+            for (int i = 0; i < jadata.size(); i++) {
+                JSONObject joindata = (JSONObject) jadata.get(i);
+                Object[] objindata = new Object[lsdatarincian.size()];
+                idlistrincian.add(String.valueOf(joindata.get("id")));
+                idlistrinciankeltrans.add(String.valueOf(joindata.get("id_keltrans")));
+                for (int j = 0; j < objindata.length; j++) {
+                    objindata[j] = joindata.get(lsdatarincian.get(j));
+                }
+                dtmrincian.addRow(objindata);
+            }
+            /*worker = new SwingWorker<Void, Void>() {
             @Override
             protected Void doInBackground() throws Exception {
-                pane.indi.setVisible(true);
-                JSONParser jpdata = new JSONParser();
-                String param = String.format("id=%s", id);
-                Object objdata = jpdata.parse(ch.getdatadetails("daftarcicilanhutangpersupplier", param));
-                JSONArray jadata = (JSONArray) objdata;
-                dtmrincian.setRowCount(0);
-                for (int i = 0; i < jadata.size(); i++) {
-                    JSONObject joindata = (JSONObject) jadata.get(i);
-                    Object[] objindata = new Object[lsdatarincian.size()];
-                    idlistrincian.add(String.valueOf(joindata.get("id")));
-                    idlistrinciankeltrans.add(String.valueOf(joindata.get("id_keltrans")));
-                    for (int j = 0; j < objindata.length; j++) {
-                        objindata[j] = joindata.get(lsdatarincian.get(j));
-                    }
-                    dtmrincian.addRow(objindata);
-                    Thread.sleep(10000);
-                }
-                
-                return null;
+            pane.indi.setVisible(true);
+            JSONParser jpdata = new JSONParser();
+            String param = String.format("id=%s", id);
+            Object objdata = jpdata.parse(ch.getdatadetails("daftarcicilanhutangpersupplier", param));
+            JSONArray jadata = (JSONArray) objdata;
+            dtmrincian.setRowCount(0);
+            for (int i = 0; i < jadata.size(); i++) {
+            JSONObject joindata = (JSONObject) jadata.get(i);
+            Object[] objindata = new Object[lsdatarincian.size()];
+            idlistrincian.add(String.valueOf(joindata.get("id")));
+            idlistrinciankeltrans.add(String.valueOf(joindata.get("id_keltrans")));
+            for (int j = 0; j < objindata.length; j++) {
+            objindata[j] = joindata.get(lsdatarincian.get(j));
+            }
+            dtmrincian.addRow(objindata);
+            }
+            
+            return null;
             }
             
             @Override
             protected void done() {
-                pane.indi.setVisible(false);
-                pane.tabledatarincian.setModel(dtmrincian);
-                disablebutton2();
-                //pane.tabledatarincian.changeSelection(rowselect, 0, false, false);
+            pane.indi.setVisible(false);
+            pane.tabledatarincian.setModel(dtmrincian);
+            disablebutton2();
+            //pane.tabledatarincian.changeSelection(rowselect, 0, false, false);
             }
             
-        };
-        worker.execute();
-        
+            };
+            worker.execute();*/
+        } catch (ParseException ex) {
+            Logger.getLogger(DaftarhutangrincianinnerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
     private void loaddatadetail() {
         pane.tcari.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
             }
-            
+
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     loaddatadetailraw();
                 }
             }
-            
+
             @Override
             public void keyReleased(KeyEvent e) {
-                
+
             }
         });
     }
-    
+
     private void selectdata() {
         pane.tabledata.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
@@ -312,16 +330,16 @@ public class DaftarhutangrincianinnerController {
                         String ids = idlist.get(row);
                         loaddatadetailrincian(ids);
                         enablebutton();
-                        
+
                     }
                 } else {
                     pane.tabledata.setCellSelectionEnabled(false);
                 }
-                
+
             }
         });
     }
-    
+
     private void selectdata2() {
         pane.tabledatarincian.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
@@ -332,18 +350,18 @@ public class DaftarhutangrincianinnerController {
             }
         });
     }
-    
+
     private void cleardata() {
         idlist.clear();
         Staticvar.ids = "";
     }
-    
+
     private void cleardata2() {
         idlistrincian.clear();
         idlistrinciankeltrans.clear();
         Staticvar.ids = "";
     }
-    
+
     private void updateloaddata() {
         pane.bupdate.addActionListener(new ActionListener() {
             @Override
@@ -354,21 +372,21 @@ public class DaftarhutangrincianinnerController {
             }
         });
     }
-    
+
     private void oncarienter() {
         pane.tcari.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
                 pane.tcari.setText("");
             }
-            
+
             @Override
             public void focusLost(FocusEvent e) {
                 //pane.tcari.setText("Cari Data");
             }
         });
     }
-    
+
     private void onbuttoncari() {
         pane.bcari.addActionListener(new ActionListener() {
             @Override
@@ -379,27 +397,27 @@ public class DaftarhutangrincianinnerController {
             }
         });
     }
-    
+
     private void disablebutton() {
         pane.bedit.setEnabled(false);
         pane.bhapus.setEnabled(false);
     }
-    
+
     private void enablebutton() {
         pane.bedit.setEnabled(true);
         pane.bhapus.setEnabled(true);
     }
-    
+
     private void disablebutton2() {
         pane.bedit2.setEnabled(false);
         pane.bhapus2.setEnabled(false);
     }
-    
+
     private void enablebutton2() {
         pane.bedit2.setEnabled(true);
         pane.bhapus2.setEnabled(true);
     }
-    
+
     private void inputpembayaran() {
         pane.btambah.addActionListener((ActionEvent e) -> {
             Staticvar.ids = "";
@@ -412,7 +430,7 @@ public class DaftarhutangrincianinnerController {
             Staticvar.pmp.container.repaint();
         });
     }
-    
+
     private void editpembayaran() {
         pane.bedit.addActionListener((ActionEvent e) -> {
             Staticvar.frame = "rincian_hutang";
@@ -426,7 +444,7 @@ public class DaftarhutangrincianinnerController {
             Staticvar.pmp.container.repaint();
         });
     }
-    
+
     private void editpembayarandetail() {
         pane.bedit2.addActionListener((ActionEvent e) -> {
             System.out.println(idlistrincian);
@@ -450,7 +468,7 @@ public class DaftarhutangrincianinnerController {
             Staticvar.pmp.container.repaint();
         });
     }
-    
+
     private void deleterincian() {
         pane.bhapus2.addActionListener(new ActionListener() {
             @Override
@@ -486,10 +504,10 @@ public class DaftarhutangrincianinnerController {
                         Staticvar.isupdate = false;
                     }
                 }
-                
+
             }
         });
-        
+
     }
-    
+
 }
