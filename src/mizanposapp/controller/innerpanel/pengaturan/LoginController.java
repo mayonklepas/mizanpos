dfe1790515493a94150d50970fe6fc51131610a3
mayonklepas <imonks21@gmail.com>
2019-03-23 12:06:37 +0800

    Merge origin/master

---
+++ src/mizanposapp/controller/innerpanel/pengaturan/LoginController.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mizanposapp.controller.innerpanel.pengaturan;
+
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import mizanposapp.helper.CrudHelper;
+import mizanposapp.helper.Staticvar;
+import mizanposapp.view.Mainmenu;
+import mizanposapp.view.frameform.Errorpanel;
+import mizanposapp.view.innerpanel.pengaturan.Login_panel;
+
+/**
+ *
+ * @author Minami
+ */
+public class LoginController {
+    
+    Login_panel pane;
+    CrudHelper ch = new CrudHelper();
+    public static String username = "";
+    
+    public LoginController(Login_panel pane) {
+        this.pane = pane;
+        loaddata();
+        login();
+    }
+    
+    private void loaddata() {
+        if (username.equals("")) {
+            pane.edusername.setEnabled(true);
+        } else {
+            pane.edusername.setText(username);
+            pane.edusername.setEnabled(false);
+            username = "";
+        }
+        
+    }
+    
+    private void login() {
+        pane.blogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rawlogin();
+            }
+        });
+        
+        pane.edpassword.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    rawlogin();
+                }
+            }
+            
+        });
+        
+        pane.edusername.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    pane.edpassword.requestFocus();
+                }
+            }
+            
+        });
+    }
+    
+    private void rawlogin() {
+        String data = "nama=" + pane.edusername.getText().trim() + "&pwd=" + pane.edpassword + "";
+        ch.insertdata("dm/cekpengguna", data);
+        if (Staticvar.getresult.equals("berhasil")) {
+            Staticvar.isupdate = true;
+            JDialog jd = (JDialog) pane.getRootPane().getParent();
+            jd.dispose();
+        } else {
+            Staticvar.isupdate = false;
+            JDialog jd = new JDialog(new Mainmenu());
+            Errorpanel ep = new Errorpanel();
+            ep.ederror.setText(Staticvar.getresult);
+            jd.add(ep);
+            jd.pack();
+            jd.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+            jd.setLocationRelativeTo(null);
+            jd.setVisible(true);
+            jd.toFront();
+        }
+    }
+    
+}
